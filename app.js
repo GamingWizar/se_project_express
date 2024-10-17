33280aa186a309c5a387a02986ec73731b408e7e
Carver Hannasch <awizard1011@gmail.com>
2024-10-17 13:00:30 -0400

    set up inital models

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const app = express();
 
 const { PORT = 3001 } = process.env;
 
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
